5645a9832db9fde46828a1b946a7020719abe942
AdamMc331 <amcneilly331@gmail.com>
2016-03-26 21:44:55 -0400

    UI touch ups.

--- app/src/main/java/me/shreyasr/chatse/chat/ChatActivity.java
+++ app/src/main/java/me/shreyasr/chatse/chat/ChatActivity.java
@@ -12,6 +12,7 @@ import android.os.Looper;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -38,6 +39,10 @@ public class ChatActivity extends AppCompatActivity implements ServiceConnection
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_chat);
+
+        // Setup toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent serviceIntent = new Intent(this, IncomingEventService.class);
         this.bindService(serviceIntent, this, Context.BIND_AUTO_CREATE);
