b395bd6edc37c197eb10255ccfb2a39eccf6f67a
AdamMc331 <amcneilly331@gmail.com>
2017-07-04 19:58:04 -0400

    Updated network package for #4.

--- app/src/main/java/me/shreyasr/chatse/network/Client.kt
+++ app/src/main/java/me/shreyasr/chatse/network/Client.kt
@@ -6,17 +6,15 @@ import com.squareup.okhttp.OkHttpClient
 import com.squareup.okhttp.Request
 import java.net.*
 
-class Client internal constructor(
-        //    private static final Client instance = new Client(new OkHttpClient(),
-        //            new PersistentCookieStore(App.get()));
-        //    public static Client get() { return instance; }
-        //    public static OkHttpClient getHttpClient() { return instance.httpClient; }
-        //    public OkHttpClient getHttpClientL() { return instance.httpClient; }
-
-        val httpClient: OkHttpClient, private val cookieStore: CookieStore) {
+/**
+ * Http Client used to make requests to SE.
+ *
+ * @property[httpClient] The base OkHttpClient to use.
+ * @property[cookieStore] The cookie storage to use.
+ */
+class Client internal constructor(val httpClient: OkHttpClient, private val cookieStore: CookieStore) {
 
     init {
-
         val cookieManager = CookieManager(cookieStore, CookiePolicy.ACCEPT_ALL)
         httpClient.cookieHandler = cookieManager
 
@@ -30,36 +28,77 @@ class Client internal constructor(
         })
     }
 
+    /**
+     * Retrieves a cookie by URI string.
+     *
+     * @param[host] The host where the cookie is being stored.
+     * @param[name] The name of the cookie to retrieve.
+     *
+     * @return The content of the cookie, or an empty string if it didn't exist.
+     */
     fun getCookie(host: String, name: String): String {
-        return getCookie(URI.create(host), name)!!
+        return getCookie(URI.create(host), name)
     }
 
-    fun getCookie(uri: URI, name: String): String? {
-        for (cookie in cookieStore.get(uri)) {
-            if (cookie.name == name) {
-                return cookie.value
-            }
-        }
-        return null
+    /**
+     * Retrieves a cookie by URI.
+     *
+     * @param[uri] The URI where the cookie is being stored.
+     * @param[name] The name of the cookie to retrieve.
+     *
+     * @return The content of the cookie, or an empty string if it didn't exist.
+     */
+    fun getCookie(uri: URI, name: String): String {
+        return cookieStore.get(uri)
+                .firstOrNull { it.name == name }
+                ?.value.orEmpty()
     }
 
+    /**
+     * Stores a cookie by URI string.
+     *
+     * @param[uri] The URI where the cookie should be stored.
+     * @param[cookieName] The name of the cookie to store.
+     * @param[cookieContent] The content of the cookkie.
+     */
     fun putCookie(uri: String, cookieName: String, cookieContent: String) {
         putCookie(URI.create(uri), cookieName, cookieContent)
     }
 
+    /**
+     * Stores a cookie by URI.
+     *
+     * @param[uri] The URI where the cookie should be stored.
+     * @param[cookieName] The name of the cookie to store.
+     * @param[cookieContent] The content of the cookkie.
+     */
     fun putCookie(uri: URI, cookieName: String, cookieContent: String) {
         cookieStore.add(uri, HttpCookie(cookieName, cookieContent))
     }
 
+    /**
+     * Creates a call for the HttpClient.
+     *
+     * @param[request]
+     */
     fun newCall(request: Request): Call {
         return httpClient.newCall(request)
     }
 
     companion object {
-
+        /**
+         * The UserAgent for all calls.
+         */
         val USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64)" + "AppleWebKit/537.36 (KHTML, like Gecko)" + "Chrome/44.0.2403.155 Safari/537.36"
 
+        /**
+         * URL to the stack exchange site.
+         */
         val SITE_STACK_EXCHANGE = "http://chat.stackexchange.com"
+
+        /**
+         * URL to the stack overflow site.
+         */
         val SITE_STACK_OVERFLOW = "http://chat.stackoverflow.com"
     }
 }
