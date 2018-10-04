54b1cf757f20d2ec357fc6ceff3a74240ea26d4b
Maurício C. P. Pessoa <mauricio.ufma@gmail.com>
2018-10-04 17:42:33 -0300

    Fixed about screen
    The dev card layout was broken on some devices.

--- app/src/main/java/com/tristanwiley/chatse/views/Corner.kt
+++
@@ -1,8 +0,0 @@
-package com.tristanwiley.chatse.views
-
-/**
- * Enum that defines each corner of a view that can be modified.
- */
-enum class Corner {
-    TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT
-}
