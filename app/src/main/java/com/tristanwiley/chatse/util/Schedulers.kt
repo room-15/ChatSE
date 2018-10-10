c70a2513e3df928cb9b954e2aab5f7413a5183b8
Tim Castelijns <timcastelijns@gmail.com>
2018-10-10 10:12:13 +0200

    Delete lingering schedulers file

--- app/src/main/java/com/tristanwiley/chatse/util/Schedulers.kt
+++
@@ -1,8 +0,0 @@
-package com.tristanwiley.chatse.util
-
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-
-fun ioScheduler() = Schedulers.io()
-
-fun uiScheduler() = AndroidSchedulers.mainThread()
