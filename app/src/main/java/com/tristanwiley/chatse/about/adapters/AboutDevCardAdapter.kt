98589ff784dce997e8d76b7b899757d6e56b7ec9
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2018-12-31 22:28:14 +0530

    Migrate to androidx.
    Migrate to androidx and use material design theme.

--- app/src/main/java/com/tristanwiley/chatse/about/adapters/AboutDevCardAdapter.kt
+++ app/src/main/java/com/tristanwiley/chatse/about/adapters/AboutDevCardAdapter.kt
@@ -1,8 +1,8 @@
 package com.tristanwiley.chatse.about.adapters
 
 import android.content.Context
-import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.RecyclerView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
